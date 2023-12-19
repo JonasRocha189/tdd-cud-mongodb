a483465e3a714c0d882c6e5e613481e04c64f844
JonasRocha189 <jonas.rocha189@gmail.com>
2023-12-19 20:36:17 -0300

    initial configuration

---
+++ src/create-repositery.js
@@ -0,0 +1,3 @@
+class UserRepositery {
+  async findOneByEmail() {}
+}
---
+++ src/create-repositery.test.js
@@ -0,0 +1,25 @@
+describe('UserRepository', () => {
+  describe('findOneByEmail', () => {
+    test.todo('Should return an user jhon@doe.com')
+    test.todo('Should lanch an exception for a non existent user')
+  })
+
+  describe('insert', () => {
+    test.todo('Insert a new user')
+  })
+
+  describe('update', () => {
+    test.todo('Should update an existent user')
+    test.todo('Should lanch an exception for a non existent user')
+  })
+
+  describe('update', () => {
+    test.todo('Should remove an existent user')
+    test.todo('Should lanch an exception for a non existent user')
+  })
+
+  describe('findAll', () => {
+    test.todo('Should return an empty list of users')
+    test.todo('Should return a list with two users')
+  })
+})
